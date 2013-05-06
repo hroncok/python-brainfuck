807f010a6abedc01baf35cc4a872cd279b18966c
Miro Hrončok <miro@hroncok.cz>
2013-05-06 14:27:46 +0200

    Initial commit

---
+++ brainx.py
@@ -0,0 +1,53 @@
+﻿#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+class BrainFuck:
+    """Interpretr jazyka brainfuck."""
+    
+    def __init__(self, data, memory=b'\x00', memory_pointer=0):
+        """Inicializace interpretru brainfucku."""
+        
+        # data programu
+        self.data = data
+        
+        # inicializace proměnných
+        self.memory = memory
+        self.memory_pointer = memory_pointer
+        
+        # DEBUG a testy
+        # a) paměť výstupu
+        self.output = ''
+    
+    #
+    # pro potřeby testů
+    #
+    def get_memory(self):
+        # Nezapomeňte upravit získání návratové hodnoty podle vaší implementace!
+        return self.memory
+
+
+class BrainLoller():
+    """Třída pro zpracování jazyka brainloller."""
+    
+    def __init__(self, filename):
+        """Inicializace interpretru brainlolleru."""
+        
+        # self.data obsahuje rozkódovaný zdrojový kód brainfucku..
+        self.data = ''
+        # ..který pak předhodíme interpretru
+        self.program = BrainFuck(self.data)
+
+
+class BrainCopter():
+    """Třída pro zpracování jazyka braincopter."""
+    
+    def __init__(self, filename):
+        """Inicializace interpretru braincopteru."""
+        
+        # self.data obsahuje rozkódovaný zdrojový kód brainfucku..
+        self.data = ''
+        # ..který pak předhodíme interpretru
+        self.program = BrainFuck(self.data)
+
+
